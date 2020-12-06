85d757cf62896b54a9a06ea014ee1fcef1ece71c
Eundo Lee <cobed95@gmail.com>
2020-12-06 14:56:06 +0900

    Remove unnecessary code from ma.py and restructure code

--- ma.py
+++ ma.py
@@ -2,79 +2,74 @@ import cv2
 import numpy as np
 
 
-def log_trans(x, k, t):
-    return ((2.0 * t + 2 * t / 50) / (
-                ((2.0 * t + 2 * t / 50) / (-t)) * np.log((2 * k * t + t / 50) / (t / 50)))) * np.log(
-        (2 * k * t + t / 50 - k * x) / (k * x + t / 50)) + t
+def get_hsv(input_path):
+    def read_image_bgr(path):
+        return cv2.imread(path)
 
+    def get_hsv_from_bgr(img_to_convert):
+        return cv2.cvtColor(img_to_convert, cv2.COLOR_BGR2HSV)
 
-def read_image_bgr(path):
-    return cv2.imread(path)
-
-
-def read_image_grayscale(path):
-    return cv2.imread(path, 0)
-
-
-def get_hsv_from_bgr(img_to_convert):
-    return cv2.cvtColor(img_to_convert, cv2.COLOR_BGR2HSV)
-
-
-def get_images(input_path):
-    input_img_bgr = read_image_bgr(input_path)
-    input_img_grayscale = read_image_grayscale(input_path)
-    input_img_hsv = get_hsv_from_bgr(input_img_bgr)
-
-    return [input_img_bgr, input_img_grayscale, input_img_hsv]
-
-
-def get_hsv(images):
-    [_, _, hsv] = images
-    return hsv
-
-
-def get_v(images):
-    [_, _, hsv] = images
-    [_, _, v] = cv2.split(hsv)
-    return v
+    img_bgr = read_image_bgr(input_path)
+    img_hsv = get_hsv_from_bgr(img_bgr)
+    return cv2.split(img_hsv)
 
 
 def apply_otsu_threshold(img_to_threshold):
     return cv2.threshold(img_to_threshold, 0, 255, cv2.THRESH_OTSU)
 
 
-def normalize(img_to_normalize):
-    return img_to_normalize / 255
+# def show_scaled(name, img):
+#     resized = cv2.resize(img, dsize=(0, 0), fx=0.2, fy=0.2)
+#     cv2.imshow(name, resized)
 
 
-def threshold_each_channel(img_to_threshold):
-    if len(img_to_threshold.shape) < 3:
-        channels = [img_to_threshold]
-    else:
-        channel_0, channel_1, channel_2 = cv2.split(img_to_threshold)
-        channels = [channel_0, channel_1, channel_2]
+def enhance(k, v_original, threshold_v, v_after_threshold):
+    def log_trans(x, k, t):
+        return ((2.0 * t + 2 * t / 50) / (
+                ((2.0 * t + 2 * t / 50) / (-t)) * np.log((2 * k * t + t / 50) / (t / 50)))) * np.log(
+            (2 * k * t + t / 50 - k * x) / (k * x + t / 50)) + t
 
-    return [apply_otsu_threshold(channel) for channel in channels]
+    def normalize(img_to_normalize):
+        return img_to_normalize / 255
+
+    def invert(image_to_invert):
+        return 255 - image_to_invert
+
+    def get_bright_area_v():
+        return normalize(v_after_threshold) * v_original
+
+    def get_dark_area_v():
+        return normalize(invert(v_after_threshold)) * v_original
+
+    bright_area_v = get_bright_area_v()
+    dark_area_v = get_dark_area_v()
+    dark_area_v_enhanced = log_trans(dark_area_v, k, threshold_v)
+    v_enhanced = (bright_area_v + dark_area_v_enhanced).astype(v_original.dtype)
+
+    return v_enhanced
+
+
+def get_bgr_from_hsv(h, s, v):
+    img_hsv = np.dstack([h, s, v])
+    return cv2.cvtColor(img_hsv, cv2.COLOR_HSV2BGR)
+
+
+def write_img(output_path, img):
+    cv2.imwrite(output_path, img)
 
 
 def main():
-    file_name = 'input006.jpeg'
+    file_name = 'input002.png'
     input_path = f'input/{file_name}'
-    images = get_images(input_path)
-    img_hsv = get_hsv(images)
-    h, s, v = cv2.split(img_hsv)
-    thresholded = [threshold_each_channel(image) for image in images]
-    [_, _, img_hsv_threshed] = thresholded
-    [_, _, channel_v_threshed] = img_hsv_threshed
-    threshold_v, img_otsu_v = channel_v_threshed
 
-    v1 = (img_otsu_v / 255) * v
-    v2 = ((255 - img_otsu_v) / 255) * v
-    v2 = log_trans(v2, 1, threshold_v)
-    v3 = (v1 + v2).astype(img_hsv.dtype)
-    hsv_img_updata = np.dstack([h, s, v3])
-    rgb = cv2.cvtColor(hsv_img_updata, cv2.COLOR_HSV2BGR)
-    cv2.imwrite(f'output/{file_name}', rgb)
+    h, s, v = get_hsv(input_path)
+    threshold_v, v_after_threshold = apply_otsu_threshold(v)
+    v_enhanced = enhance(k=1, v_original=v, threshold_v=threshold_v, v_after_threshold=v_after_threshold)
+
+    img_bgr_enhanced = get_bgr_from_hsv(h, s, v_enhanced)
+
+    output_path = f'output/{file_name}'
+    write_img(output_path, img_bgr_enhanced)
 
 
 if __name__ == '__main__':
