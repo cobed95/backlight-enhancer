65bcbae59b01d83cd1f95ab8f516eddeeabb3f55
Eundo Lee <cobed95@gmail.com>
2020-12-14 22:14:49 +0900

    Update code to respect some of PEP guidelines

--- histogram_specification.py
+++ histogram_specification.py
@@ -1,113 +1,114 @@
 import cv2
 import numpy as np
 
-def GenTarget_Hist(input_hist):
-	l_2 = int(len(input_hist)/2)
-	target_hist = input_hist.copy()
-	Sum = 0
-	for i in range(0, l_2):
-		Sum = Sum+input_hist[i]
-	h = 2*Sum/l_2
 
-	for i in range(0, l_2):
-		target_hist[i][0] = h*i/l_2
-	return target_hist
+def GenTarget_Hist(input_hist):
+    l_2 = int(len(input_hist) / 2)
+    target_hist = input_hist.copy()
+    Sum = 0
+    for i in range(0, l_2):
+        Sum = Sum + input_hist[i]
+    h = 2 * Sum / l_2
+
+    for i in range(0, l_2):
+        target_hist[i][0] = h * i / l_2
+    return target_hist
+
 
 def intensity(img):
-	R = np.array([img.T[0]]).T
-	G = np.array([img.T[1]]).T
-	B = np.array([img.T[2]]).T
-	I_float = np.around(R/3+G/3+B/3)
-	I = np.zeros(shape = I_float.shape, dtype = 'uint8')
-	for i, r in enumerate(I_float):
-		for j, e in enumerate(r):
-			I[i][j][0] = int(e[0])
-	return I
+    R = np.array([img.T[0]]).T
+    G = np.array([img.T[1]]).T
+    B = np.array([img.T[2]]).T
+    I_float = np.around(R / 3 + G / 3 + B / 3)
+    I = np.zeros(shape=I_float.shape, dtype='uint8')
+    for i, r in enumerate(I_float):
+        for j, e in enumerate(r):
+            I[i][j][0] = int(e[0])
+    return I
+
 
 def calAlpha(G, I_mean):
-	alpha = np.zeros(shape = (G.shape[0], G.shape[1], 1))
-	for i in range(0,G.shape[0]):
-		for j in range(0,G.shape[1]):
-			t = np.abs(G[i][j]-I_mean)/I_mean
-			alpha[i][j] = [t] if 1>t else [1]
-	return alpha
+    alpha = np.zeros(shape=(G.shape[0], G.shape[1], 1))
+    for i in range(0, G.shape[0]):
+        for j in range(0, G.shape[1]):
+            t = np.abs(G[i][j] - I_mean) / I_mean
+            alpha[i][j] = [t] if 1 > t else [1]
+    return alpha
 
 
-def enhance(img):	
-	print('Calculate intensity')	
-	# Calculate intensity of img
-	I = intensity(img)
+def enhance(img):
+    print('Calculate intensity')
+    # Calculate intensity of img
+    I = intensity(img)
 
-	print('Calculate alpha')	
-	# Calculate alpha
-	G = cv2.GaussianBlur(I, (11,11), 0)
-	I_mean = np.mean(I)
-	alpha = calAlpha(G, I_mean)
-	
-	print('Generate Histogram')
-	# Calculate the iinput images' histogram
-	input_hist = cv2.calcHist([I], [0], None, [256], [0,256])
+    print('Calculate alpha')
+    # Calculate alpha
+    G = cv2.GaussianBlur(I, (11, 11), 0)
+    I_mean = np.mean(I)
+    alpha = calAlpha(G, I_mean)
 
-	#Normalize the input histogram
-	total = sum(input_hist)
-	input_hist /= total
-	
-	# Calculate the cumulateive input histogram
-	cum_input_hist = []
-	cum = 0.0
-	for i in range(0, len(input_hist)):
-		cum+=input_hist[i][0]
-		cum_input_hist.append(cum)
+    print('Generate Histogram')
+    # Calculate the iinput images' histogram
+    input_hist = cv2.calcHist([I], [0], None, [256], [0, 256])
 
-	print('Generate target Histogram and lookup table')
-	#Generate target_histograms
-	target_hist = GenTarget_Hist(input_hist)
-	
-	# Calculate the cumulative target histogram
-	cum_target_hist = []
-	cum = 0.0
-	for j in range(0, len(target_hist)):
-		cum+=target_hist[j][0]
-		cum_target_hist.append(cum)
-	
+    # Normalize the input histogram
+    total = sum(input_hist)
+    input_hist /= total
 
-	# Obtain the mapping from the input hist to target hist
-	lookup = {}
-	for i in range(len(cum_input_hist)):
-		min_val = abs(cum_target_hist[0] - cum_input_hist[i])
-		min_j=0
-		
-		for j in range(1, len(cum_target_hist)):
-			val = abs(cum_target_hist[j] - cum_input_hist[i])
-			if (val < min_val):
-				min_val = val
-				min_j = j
-		lookup[i] = min_j
+    # Calculate the cumulateive input histogram
+    cum_input_hist = []
+    cum = 0.0
+    for i in range(0, len(input_hist)):
+        cum += input_hist[i][0]
+        cum_input_hist.append(cum)
 
-	print('Intensity calcuation')
-	# Create the transformed image using the img's pixel values and the lookup table
-	trans_img = img.copy()
-	I_HS = I.copy()
-	for i in range(img.shape[0]):
-		for j in range(img.shape[1]):
-			I_HS[i][j][0] = lookup[I[i][j][0]]
-	
-	I_ = alpha*I_HS+(1-alpha)*I
-	
-	for i, r in enumerate(I):
-		for j, e in enumerate(r):
-			if e[0] ==0: 
-				I[i][j][0] = 1
-	
-	ratio = I_/I
+    print('Generate target Histogram and lookup table')
+    # Generate target_histograms
+    target_hist = GenTarget_Hist(input_hist)
 
-	print('Color image convertion')
-	t = ratio*img
-	M = np.max(t)
-	r = 255/M
-	for i in range(img.shape[0]):
-		for j in range(img.shape[1]):
-			for k in range(img.shape[2]):
-				trans_img[i][j][k] = int(t[i][j][k]*r)
-	return trans_img	
+    # Calculate the cumulative target histogram
+    cum_target_hist = []
+    cum = 0.0
+    for j in range(0, len(target_hist)):
+        cum += target_hist[j][0]
+        cum_target_hist.append(cum)
 
+    # Obtain the mapping from the input hist to target hist
+    lookup = {}
+    for i in range(len(cum_input_hist)):
+        min_val = abs(cum_target_hist[0] - cum_input_hist[i])
+        min_j = 0
+
+        for j in range(1, len(cum_target_hist)):
+            val = abs(cum_target_hist[j] - cum_input_hist[i])
+            if (val < min_val):
+                min_val = val
+                min_j = j
+        lookup[i] = min_j
+
+    print('Intensity calcuation')
+    # Create the transformed image using the img's pixel values and the lookup table
+    trans_img = img.copy()
+    I_HS = I.copy()
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            I_HS[i][j][0] = lookup[I[i][j][0]]
+
+    I_ = alpha * I_HS + (1 - alpha) * I
+
+    for i, r in enumerate(I):
+        for j, e in enumerate(r):
+            if e[0] == 0:
+                I[i][j][0] = 1
+
+    ratio = I_ / I
+
+    print('Color image convertion')
+    t = ratio * img
+    M = np.max(t)
+    r = 255 / M
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            for k in range(img.shape[2]):
+                trans_img[i][j][k] = int(t[i][j][k] * r)
+    return trans_img
--- pyramid_fusion.py
+++ pyramid_fusion.py
@@ -5,137 +5,145 @@ import numpy as np
 
 
 def normalize_input(I):
-  _min = np.min(I)
-  _max = np.max(I)
-  mag = _max - _min
-  normalized = (I - _min) / mag
-  return normalized
+    _min = np.min(I)
+    _max = np.max(I)
+    mag = _max - _min
+    normalized = (I - _min) / mag
+    return normalized
+
 
 def build_W(I):
-  sigma = 0.3
-  W = np.exp(-(((I - 0.5) ** 2) / (2 * sigma * sigma)))
-  return W
+    sigma = 0.3
+    W = np.exp(-(((I - 0.5) ** 2) / (2 * sigma * sigma)))
+    return W
+
 
 def build_norm_Ws(W1, W2):
-  norm_W1 = np.zeros(W1.shape).astype(np.float64)
-  norm_W2 = np.zeros(W1.shape).astype(np.float64)
+    norm_W1 = np.zeros(W1.shape).astype(np.float64)
+    norm_W2 = np.zeros(W1.shape).astype(np.float64)
 
-  Y, X = W1.shape
+    Y, X = W1.shape
 
-  for y in range(Y):
-    for x in range(X):
-      total = W1[y, x] + W2[y, x]
+    for y in range(Y):
+        for x in range(X):
+            total = W1[y, x] + W2[y, x]
 
-      norm_W1[y, x] = W1[y, x] / total
-      norm_W2[y, x] = W2[y, x] / total
+            norm_W1[y, x] = W1[y, x] / total
+            norm_W2[y, x] = W2[y, x] / total
 
-  return norm_W1, norm_W2
+    return norm_W1, norm_W2
+
 
 def build_I1(V):
-  alpha = 0.5
+    alpha = 0.5
 
-  V_clone = V[:,:].astype(np.float64)
-  V_clone += 1
-  I1 = np.log(alpha * V_clone.astype(np.float64))
+    V_clone = V[:, :].astype(np.float64)
+    V_clone += 1
+    I1 = np.log(alpha * V_clone.astype(np.float64))
 
-  return I1
+    return I1
+
 
 def build_I2(V):
-  def compute_gamma():
-    Y, X = V.shape
-    N = Y * X
-    n = 0
-    for y in range(Y):
-      for x in range(X):
-        if V[y, x] < 50:
-          n += 1
-    return (N - n) / N
-  
-  gamma = compute_gamma()
+    def compute_gamma():
+        Y, X = V.shape
+        N = Y * X
+        n = 0
+        for y in range(Y):
+            for x in range(X):
+                if V[y, x] < 50:
+                    n += 1
+        return (N - n) / N
 
-  V_clone = V[:,:].astype(np.float64)
-  I2 = 255 - (255 - V_clone) ** gamma
+    gamma = compute_gamma()
 
-  return I2
+    V_clone = V[:, :].astype(np.float64)
+    I2 = 255 - (255 - V_clone) ** gamma
+
+    return I2
+
 
 def fusion(norm_I1, norm_I2, norm_W1, norm_W2):
-  Y, X = norm_I1.shape
+    Y, X = norm_I1.shape
 
-  depth = 5
+    depth = 5
 
-  g_W1, _ = build_pyramids(norm_W1, depth)
-  g_W2, _ = build_pyramids(norm_W2, depth)
-  _, l_I1 = build_pyramids(norm_I1, depth)
-  _, l_I2 = build_pyramids(norm_I2, depth)
+    g_W1, _ = build_pyramids(norm_W1, depth)
+    g_W2, _ = build_pyramids(norm_W2, depth)
+    _, l_I1 = build_pyramids(norm_I1, depth)
+    _, l_I2 = build_pyramids(norm_I2, depth)
 
-  V = [np.zeros((1, 1))] * depth
+    V = [np.zeros((1, 1))] * depth
 
-  for d in range(depth):
-    V[d] = np.zeros(g_W1[d].shape).astype(np.float64)
-    Y, X = V[d].shape
+    for d in range(depth):
+        V[d] = np.zeros(g_W1[d].shape).astype(np.float64)
+        Y, X = V[d].shape
 
-    for y in range(Y):
-      for x in range(X):
-        V[d][y, x] = g_W1[d][y, x] * l_I1[d][y, x] + g_W2[d][y, x] * l_I2[d][y, x]
+        for y in range(Y):
+            for x in range(X):
+                V[d][y, x] = g_W1[d][y, x] * l_I1[d][y, x] + g_W2[d][y, x] * l_I2[d][y, x]
 
-  for d in range(depth - 2, -1, -1):
-    shape = V[d].shape
-    V[d] = cv2.add(V[d], cv2.pyrUp(V[d + 1], dstsize=(shape[1], shape[0])))
-  
-  return V[0]
+    for d in range(depth - 2, -1, -1):
+        shape = V[d].shape
+        V[d] = cv2.add(V[d], cv2.pyrUp(V[d + 1], dstsize=(shape[1], shape[0])))
+
+    return V[0]
+
 
 def get_shape(I, depth):
-  if depth == 0:
-    return I.shape 
-  else:
-    Y, X = get_shape(I, depth - 1) 
-    return ((Y + 1) // 2, (X + 1) // 2)
+    if depth == 0:
+        return I.shape
+    else:
+        Y, X = get_shape(I, depth - 1)
+        return ((Y + 1) // 2, (X + 1) // 2)
+
 
 def build_pyramids(I, depth=5):
-  gaussian_pyramids = [I]
+    gaussian_pyramids = [I]
 
-  cur = I
-  for d in range(1, depth):
-    shape = get_shape(I, d)
-    new = cv2.pyrDown(cur)
-    gaussian_pyramids.append(new)
-    cur = new
+    cur = I
+    for d in range(1, depth):
+        shape = get_shape(I, d)
+        new = cv2.pyrDown(cur)
+        gaussian_pyramids.append(new)
+        cur = new
 
-  laplacian_pyramids = [gaussian_pyramids[depth - 1]]
+    laplacian_pyramids = [gaussian_pyramids[depth - 1]]
 
-  for d in range(depth - 2, -1, -1):
-    shape = get_shape(I, d)
-    gaussian_expanded = cv2.pyrUp(gaussian_pyramids[d + 1], dstsize=(shape[1], shape[0]))
-    laplacian = cv2.subtract(gaussian_pyramids[d], gaussian_expanded)
-    laplacian_pyramids.append(laplacian)
+    for d in range(depth - 2, -1, -1):
+        shape = get_shape(I, d)
+        gaussian_expanded = cv2.pyrUp(gaussian_pyramids[d + 1], dstsize=(shape[1], shape[0]))
+        laplacian = cv2.subtract(gaussian_pyramids[d], gaussian_expanded)
+        laplacian_pyramids.append(laplacian)
 
-  laplacian_pyramids = list(reversed(laplacian_pyramids))
+    laplacian_pyramids = list(reversed(laplacian_pyramids))
 
-  return gaussian_pyramids, laplacian_pyramids
+    return gaussian_pyramids, laplacian_pyramids
+
 
 def enhance(img):
-  hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-  V = hsv_img[:,:,2]
-  
-  print(np.min(V), np.max(V))
+    V = hsv_img[:, :, 2]
 
-  I1 = build_I1(V)
-  I2 = build_I2(V)
+    print(np.min(V), np.max(V))
 
-  norm_I1 = normalize_input(I1)
-  norm_I2 = normalize_input(I2)
+    I1 = build_I1(V)
+    I2 = build_I2(V)
 
-  W1 = build_W(norm_I1)
-  W2 = build_W(norm_I2)
+    norm_I1 = normalize_input(I1)
+    norm_I2 = normalize_input(I2)
 
-  norm_W1, norm_W2 = build_norm_Ws(W1, W2)
+    W1 = build_W(norm_I1)
+    W2 = build_W(norm_I2)
 
-  new_V = fusion(norm_I1, norm_I2, norm_W1, norm_W2)
-  new_V = (new_V * 255).astype(np.uint8)
+    norm_W1, norm_W2 = build_norm_Ws(W1, W2)
 
-  hsv_img[:,:,2] = new_V
+    new_V = fusion(norm_I1, norm_I2, norm_W1, norm_W2)
+    new_V = (new_V * 255).astype(np.uint8)
 
-  enhanced_img = cv2.cvtColor(hsv_img, cv2.COLOR_HSV2BGR)
-  
-  return enhanced_img
+    hsv_img[:, :, 2] = new_V
+
+    enhanced_img = cv2.cvtColor(hsv_img, cv2.COLOR_HSV2BGR)
+
+    return enhanced_img
